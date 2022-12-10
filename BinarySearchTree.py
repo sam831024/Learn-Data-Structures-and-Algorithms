31f6491602dd3cf9286b51bc2bb5257845bfc567
Sam Huang <97336355+sam831024@users.noreply.github.com>
2022-12-10 15:49:01 +0800

    Update BinarySearchTree.py

--- BinarySearchTree.py
+++ BinarySearchTree.py
@@ -1,16 +1,15 @@
 class TreeNode:
-    def __init__(self, val):
+    def _int_(self , val)：
         self.val = val
         self.left = None
         self.right = None
-
+        
     def search(root, target):
         if not root:
             return False
-
-        if target > root.val:
-            return search(root.right, target)
-        elif target < root.val:
+        elif root.val > target:
             return search(root.left, target)
+        elif root.val < target:
+            return search(root.right, target)
         else:
             return True
