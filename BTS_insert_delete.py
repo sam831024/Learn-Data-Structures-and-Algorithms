e965fffc97b8ea3378b9017308b049fa61821150
Sam Huang <97336355+sam831024@users.noreply.github.com>
2022-12-12 00:13:29 +0800

    Update BTS_insert_delete.py

--- BTS_insert_delete.py
+++ BTS_insert_delete.py
@@ -3,7 +3,18 @@ class TreeNode:
         self.val = val
         self.left = None
         self.right = None
-        
+
+# Insert a new node and return the root of the BST.
+def insert(root, val):
+    if not root:
+        return TreeNode(val)
+    
+    if val > root.val:
+        root.right = insert(root.right, val)
+    elif val < root.val:
+        root.left = insert(root.left, val)
+    return root
+
 # Return the minimum value node of the BST.
 def minValueNode(root):
     curr = root
